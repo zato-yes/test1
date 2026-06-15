a4bbb9d267b92ccfdf9c582e5155974ff6cbfa8c
zato-yes <denizhh@proton.me>
2026-06-15 03:06:33 +0200

    Update maingame.lua

--- maingame.lua
+++ maingame.lua
@@ -225,7 +225,7 @@ local bosses = {
     			print("Found:", target.Name)
 					task.wait(7)
 				workspace.Map.Presets.AlienMothership.Outside.UfoBottom:WaitForChild("Entry"):GetPivot()
-    			rootPart.CFrame = workspace.Map.Presets.AlienMothership.Outside.UfoBottom.Entry.CFrame * CFrame.new(0, -6, 0)
+    			rootPart.CFrame = workspace.Map.Presets.AlienMothership.Outside.UfoBottom.Entry.CFrame * CFrame.new(0, -15, 0)
 					task.wait(14)
 						workspace.Map.Presets.AlienMothership.BossArena:WaitForChild("Platform"):GetPivot()
         				rootPart.CFrame = workspace.Map.Presets.AlienMothership.BossArena.Platform:GetPivot()
@@ -234,7 +234,7 @@ local bosses = {
         end
     },
     {
-        name = "[C.O.R.E]",
+        name = "C.O.R.E",
         onDeath = function()
             task.wait(4)
             rootPart.CFrame = CFrame.new(21, 6, -40014)

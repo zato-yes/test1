89528b1b18a84e7e6c5255fc86e552e553f43fe9
zato-yes <denizhh@proton.me>
2026-06-18 01:09:43 +0200

    Update maingame.lua

--- maingame.lua
+++ maingame.lua
@@ -1,18 +1,8 @@
 
 local placeId = game.PlaceId
 
-if placeId == 104770044244450 then
-    print("lobby")
-    end
-    
+if placeId == 104770044244450 then return end
 
-elseif placeId == 89744231770777 then
-    print("maingame") 
-    	
-
-else
-    print("unknown")
-end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 task.wait(0.5)
